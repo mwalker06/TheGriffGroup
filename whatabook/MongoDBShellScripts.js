a71ecfdf7a799f65b865174818312e53fdba3d85
mwalker06 <81312304+mwalker06@users.noreply.github.com>
2023-05-07 11:00:51 -0500

    made changes to python script and both js documents. used bulk actions instead of single actions. got rid of os.system (pause) because not all users run windows

--- whatabook/MongoDBShellScripts.js
+++ whatabook/MongoDBShellScripts.js
@@ -6,149 +6,157 @@
 ; Modified By: Walker, Gopin, and Brooks
 ; Description:  MongoDB Shell Scripts for books and customers collections.
 ============================================
-*/ 
+*/
 
+const booksData = [
+  {
+    title: "The Catcher in the Rye",
+    genre: "Fiction",
+    author: "J.D. Salinger",
+    bookId: "1234",
+  },
+  {
+    title: "To Kill a Mockingbird",
+    genre: "Fiction",
+    author: "Harper Lee",
+    bookId: "91011",
+  },
+  {
+    title: "The Great Gatsby",
+    genre: "Fiction",
+    author: "F. Scott Fitzgerald",
+    bookId: "121314",
+  },
+  {
+    title: "MongoDB The Definitive Guide",
+    genre: "Non-Fiction",
+    author: "Shannon Bradshaw",
+    bookId: "954461",
+  },
+];
 
-// Delete the books and customers collections.
-db.books.drop();
-db.customers.drop();
-
-// Create the books collection using Document Validation.
-db.createCollection("books", {
-    validator: { $jsonSchema: {
-        bsonType: "object",
-        properties: {
-            title: {
-                bsonType: "string"
-            },
-            genre: {
-                bsonType: "string"
-            },
-            author: {
-                bsonType: "string"
-            },
-            bookId: {
-                bsonType: "string"
-            }
-        }
-    }}
-});
-
-// Create the customers collection using Document Validation.
-db.createCollection("customers", {
-    validator: { $jsonSchema: {
-        bsonType: "object",
-        properties: {
-            firstName: {
-                bsonType: "string"
-            },
-            lastName: {
-                bsonType: "string"
-            },
-            customerId: {
-                bsonType: "string"
-            },
-            wishlist: {
-                bsonType: "array"
-            },
-        }
-    }}
-});
-
-
-
-
-// Books
-catcherInTheRye = {
-    "title": "The Catcher in the Rye",
-    "genre": "Fiction",
-    "author": "J.D. Salinger",
-    "bookId": "1234"  
-}
-
-toKillAMockingbird = {
-    "title": "To Kill a Mockingbird",
-    "genre": "Fiction",
-    "author": "Harper Lee",
-    "bookId": "91011"  
-}
-
-greatGatsby = {
-    "title": "The Great Gatsby",
-    "genre": "Fiction",
-    "author": "F. Scott Fitzgerald",
-    "bookId": "121314"  
-}
-
-mongoDG = {
-    "title": "MongoDB The Definitive Guide",
-    "genre": "Non-Fiction",
-    "author": "Shannon Bradshaw",
-    "bookId": "954461"
-}
-
-// Insert the documents.
-db.books.insertOne(catcherInTheRye);
-db.books.insertOne(toKillAMockingbird);
-db.books.insertOne(greatGatsby);
-db.books.insertOne(mongoDG);
-
-
-
-// Customers
-alice = {
-    "firstName": "Alice",
-    "lastName": "Smith",
-    "customerId": "c1007",
-    "wishlist": [{
+const customersData = [
+  {
+    firstName: "Alice",
+    lastName: "Smith",
+    customerId: "c1007",
+    wishlist: [
+      {
         title: "The Catcher in the Rye",
         genre: "Fiction",
         author: "J.D. Salinger",
-        bookId: "1234" 
-    },]
-}
-
-karen = {
-    "firstName": "Karen",
-    "lastName": "Smith",
-    "customerId": "c1008",
-    "wishlist": [{
+        bookId: "1234",
+      },
+    ],
+  },
+  {
+    firstName: "Karen",
+    lastName: "Smith",
+    customerId: "c1008",
+    wishlist: [
+      {
         title: "To Kill a Mockingbird",
         genre: "Fiction",
         author: "Harper Lee",
-        bookId: "91011" 
-    },{
+        bookId: "91011",
+      },
+      {
         title: "The Catcher in the Rye",
         genre: "Fiction",
         author: "J.D. Salinger",
-        bookId: "1234" 
-    },]    
-}
-
-ashley = {
-    "firstName": "Ashley",
-    "lastName": "Smith",
-    "customerId": "c1009",
-    "wishlist": [{
+        bookId: "1234",
+      },
+    ],
+  },
+  {
+    firstName: "Ashley",
+    lastName: "Smith",
+    customerId: "c1009",
+    wishlist: [
+      {
         title: "To Kill a Mockingbird",
         genre: "Fiction",
         author: "Harper Lee",
-        bookId: "91011" 
-    },{
+        bookId: "91011",
+      },
+      {
         title: "The Catcher in the Rye",
         genre: "Fiction",
         author: "J.D. Salinger",
-        bookId: "1234" 
-    },{
+        bookId: "1234",
+      },
+      {
         title: "MongoDB The Definitive Guide",
         genre: "Non-Fiction",
         author: "Shannon Bradshaw",
-        bookId: "954461"
-    },]    
+        bookId: "954461",
+      },
+    ],
+  },
+];
+
+// Insert documents using bulk write operation
+function insertDocuments(collection, documents) {
+  const bulkOperations = documents.map((doc) => ({
+    insertOne: { document: doc },
+  }));
+  return collection.bulkWrite(bulkOperations);
 }
 
-// Insert the document. 
-db.customers.insertOne(alice);
-db.customers.insertOne(karen);
-db.customers.insertOne(ashley);
+// Delete existing collections
+db.books.drop();
+db.customers.drop();
 
+// Create the books collection with validation
+db.createCollection("books", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      properties: {
+        title: { bsonType: "string" },
+        genre: { bsonType: "string" },
+        author: { bsonType: "string" },
+        bookId: { bsonType: "string" },
+      },
+    },
+  },
+});
+
+// Create the customers collection with validation
+db.createCollection("customers", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      properties: {
+        firstName: { bsonType: "string" },
+        lastName: { bsonType: "string" },
+        customerId: { bsonType: "string" },
+        wishlist: { bsonType: "array" },
+      },
+    },
+  },
+});
+
+// Get references to the collections
+const booksCollection = db.books;
+const customersCollection = db.customers;
+
+// Insert documents into the collections
+insertDocuments(booksCollection, booksData)
+  .then(() => {
+    // Insert the book documents completed successfully
+    console.log("Book documents inserted successfully.");
+
+    // Proceed to insert customer documents
+    return insertDocuments(customersCollection, customersData);
+  })
+  .then(() => {
+    // Insert the customer documents completed successfully
+    console.log("Customer documents inserted successfully.");
+
+    // Perform additional operations or return a response if needed
+  })
+  .catch((error) => {
+    // Handle any errors that occurred during the insertion
+    console.error("Error inserting documents:", error);
+  });
--- whatabook/TheGriffGroup_WhatABook.py
+++ whatabook/TheGriffGroup_WhatABook.py
@@ -6,106 +6,76 @@
     Description: Python Console Application
 
 """
-
+# import statements
 import os
 
-# import the MongoClient
+# import statements for MongoDB
 from pymongo import MongoClient
 
-# build a connection string to connect to
+
+# configure the connection to the MongoDB cluster
 client = MongoClient('mongodb+srv://web335_user:s3cret@bellevueuniversity.kqpr8ra.mongodb.net/?retryWrites=true&w=majority')
 
 # configure a variable to access the web335DB
 db = client['web335DB']
+books = db.books
+customers = db.customers
 
-
-
-# Display a list of books. Format the output so it is easy to read.
-def books():
-
-    books = db.books.find({}, {'_id':0,'title':1,'genre':1,'author':1})
-
-    for book in books:
+# Display a list of books
+def display_books():
+    for book in books.find({}, {'_id': 0, 'title': 1, 'genre': 1, 'author': 1}):
         print(f"\n{book}")
+    input("\nPress enter to continue...")
 
-    os.system('pause')
-
-
-
-# Display a list of books by Genre. For this requirement, supply the user 
-# with a list of genre choices and display the appropriate books based on 
-# their selection.
-def genre():
-
+# Display a list of books by genre
+def display_books_by_genre():
     selection = input(f"\n\t-'fiction'\n\t-'non-fiction'\n\nPlease make a selection: ")
-
     if selection == 'quit':
         active = False
-
-    elif selection == 'fiction':
-        for book in db.books.find({'genre': 'Fiction'},{'_id':0,'title':1, 'author': 1}):
-            
+    elif selection in ['fiction', 'non-fiction']:
+        for book in books.find({'genre': selection.capitalize()}, {'_id': 0, 'title': 1, 'author': 1}):
             print(f"\n{book}")
-
-        os.system('pause')
-
-    elif selection == 'non-fiction':
-        for book in db.books.find({'genre': 'Non-Fiction'},{'_id':0,'title':1, 'author': 1}):
-            
-            print(f"\n{book}")
-
-        os.system('pause')
-
+        input("\nPress enter to continue...")
     else:
-        print(f"\n\t{(selection.upper())}, is an invalid selection.\n\tPlease try your selection again.\n")
+        print(f"\n\t'{selection}' is an invalid selection.\n\tPlease try again.\n")
+        input("\nPress enter to continue...")
 
-        os.system('pause')
-
-
-
-# Display a customers wishlist by customerId. For this requirement, prompt the 
-# user to enter a customerId (c1007, c1008, or c1009) and display the 
-# appropriate wishlist.
-def search():
-
-    user = input("please enter your customerId: ")
-
-    if db.customers.count_documents({"customerId": user}) > 0:
-        for book in db.customers.find({"customerId": user},{'_id':0,'firstName':1, 'lastName': 1, 'wishlist.title': True, 'wishlist.author': True}):
-            print(book)
-
-            os.system('pause')
-
+# Display a customer's wishlist by customerId
+def display_wishlist_by_customer():
+    user = input("Please enter your customerId: ")
+    customer = customers.find_one({"customerId": user})
+    if customer:
+        print(customer)
+        input("\nPress enter to continue...")
     else:
-        print(f"\n\t{(user.upper())} is not a registered user.\n\tPlease try again.\n")
+        print(f"\n\t'{user}' is not a registered user.\n\tPlease try again.\n")
+        input("\nPress enter to continue...")
 
-        os.system('pause')
+# Main function to call other functions based on user input
+def main():
+    active = True
+    greeting = (
+        f"\n\tWhat_A_Book\n\n\tPython_Console\n\t\nPlease make a selection:\n\n"
+        f"- To end the program, enter 'quit'\n"
+        f"- To list all books, enter 'books'\n"
+        f"- To see books listed by genre, enter 'genre'\n"
+        f"- To display a customer's wishlist by customerId, enter 'search'\n\n"
+        f"Please enter command: "
+    )
+    while active:
+        message = input(greeting)
+        if message == 'quit':
+            active = False
+        elif message == 'books':
+            display_books()
+        elif message == 'genre':
+            display_books_by_genre()
+        elif message == 'search':
+            display_wishlist_by_customer()
+        else:
+            print(f"\n\t'{message}' is not a valid command.\n\tPlease try again.\n")
+            input("\nPress enter to continue...")
 
-
-
-greeting = f"\n\tWhat_A_Book\n\n\tPython_Console\n\t\nPlease make a selection:\n\n-To end program, Enter 'quit'\n-To list all books, Enter 'books'\n-To see books listed by genre, Enter 'genre'\n-Display customer wishlist by customerId, Enter 'search'\n\nPlease enter command: "
-
-
-
-active = True
-
-while active: 
-    message = input(greeting)
-
-    if message == 'quit':
-        active = False
-
-    elif message == 'books':
-        books()
-
-    elif message == 'genre':
-        genre()
-
-    elif message == 'search':
-        search()
-
-    else: 
-        print(f"\n\t{(message.upper())}, is not a working command.\n\tPlease try again.\n")
-
-        os.system('pause')
-
+# Call the main function when the program starts
+if __name__ == '__main__':
+    main()
